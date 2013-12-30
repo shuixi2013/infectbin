4e81b4a632d63a75e6059398e4abc18bf74f9ea5
João Guilherme Victorino <jg.victorino1@gmail.com>
2013-12-30 19:01:54 -0200

    Create list.c
    functions for linked list

---
+++ list.c
@@ -0,0 +1,46 @@
+#include "list.h"
+#include <stdlib.h>
+
+void list_init(List *list, void (*destroy)(void *data)){
+	list->size = 0;
+	list->destroy = (void *) destroy;
+	list->head = NULL;
+}
+
+void list_destroy(List *list){
+	ListElmt *element;
+
+	foreach(list, element)
+		list_rem(list);
+
+	list->destroy = NULL;
+}
+
+int  list_ins(List *list, const void *data){
+	ListElmt *element;
+	element = (ListElmt *) malloc(sizeof(ListElmt));
+
+	if(element == NULL)
+		return -1;
+
+	element->data = (void*) data;
+	element->next = list->head;
+	list->head = element;
+	list->size++;
+
+	return 0;
+}
+
+int  list_rem(List *list){
+	ListElmt *element;
+
+	if(list->size == 0)
+		return -1;
+
+	element = list->head->next;
+	list->destroy(list->head);
+	list->head = element;
+	list->size--;
+
+	return 0;
+}
