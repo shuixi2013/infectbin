17ff92a5d0d19bd9dd93e1795966e826d8feda80
João Guilherme Victorino <jg.victorino1@gmail.com>
2013-12-30 18:58:55 -0200

    Create easyptrace.h

---
+++ easyptrace.h
@@ -0,0 +1,12 @@
+#ifndef EASYPTRACE_H
+#define EASYPTRACE_H
+
+#include <sys/ptrace.h>
+#include <sys/types.h>
+
+void ptrace_attach(pid_t pid);
+void ptrace_detach(pid_t pid);
+long ptrace_peektext(pid_t pid, void *addr);
+void ptrace_poketext(pid_t pid, void *addr, void *data);
+
+#endif
