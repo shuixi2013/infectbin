c81c23a0293e0e49aef9e65b532e1681279f131b
João Guilherme Victorino <jg.victorino1@gmail.com>
2013-12-30 19:03:08 -0200

    Create list.h

---
+++ list.h
@@ -0,0 +1,26 @@
+#ifndef LIST_H
+#define LIST_H
+
+
+typedef struct ListElmt_{
+	void 		 *data;
+	struct ListElmt_ *next;
+
+}ListElmt;
+
+typedef struct List_{
+	int size;
+	int (*destroy)(void *data);
+	ListElmt *head;
+
+}List;
+
+#define foreach(list, element) \
+	for(element = list->head; element != NULL; element = element->next)
+
+void list_init(List *list, void (*destroy)(void *data));
+void list_destroy(List *list);
+int  list_ins(List *list, const void *data);
+int  list_rem(List *list);
+
+#endif
