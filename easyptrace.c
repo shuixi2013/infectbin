e4efe0271c8a30707c12dabeec5a414e13a5b481
João Guilherme Victorino <jg.victorino1@gmail.com>
2014-01-24 09:03:29 -0200

    Update easyptrace.c

--- easyptrace.c
+++ easyptrace.c
@@ -1,23 +1,24 @@
 #include "easyptrace.h"
+#include <stdlib.h>
 
 void ptrace_attach(pid_t pid){
 	if(ptrace(PTRACE_ATTACH, pid, NULL, NULL) < 0){
 		perror("ptrace_attach");
-		exit(-1);
+		exit(EXIT_FAILURE);
 	}
 }
 
 void ptrace_detach(pid_t pid){
 	if(ptrace(PTRACE_DETACH, pid, NULL, NULL) < 0){
 		perror("ptrace_detach");
-		exit(-1);
+		exit(EXIT_FAILURE);
 	}
 }
 
 void ptrace_poketext(pid_t pid, void *addr, void *data){
 	if(ptrace(PTRACE_POKETEXT, pid, addr, data) < 0){
 		perror("ptrace_poketext");
-		exit(-1);
+		exit(EXIT_FAILURE);
 	}
 }
 
