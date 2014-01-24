7db52e9f325744e9d486b5873fbc6d51166ab526
João Guilherme Victorino <jg.victorino1@gmail.com>
2014-01-24 09:04:34 -0200

    Update easyptrace.h

--- easyptrace.h
+++ easyptrace.h
@@ -4,9 +4,12 @@
 #include <sys/ptrace.h>
 #include <sys/types.h>
 
+/* only necessary functions ... :p */
+
 void ptrace_attach(pid_t pid);
 void ptrace_detach(pid_t pid);
 long ptrace_peektext(pid_t pid, void *addr);
 void ptrace_poketext(pid_t pid, void *addr, void *data);
+
 
 #endif
