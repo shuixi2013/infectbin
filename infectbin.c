26d905fa6b67a854b518c60d456294ff57262b6e
João Guilherme Victorino <jg.victorino1@gmail.com>
2015-03-09 01:20:06 -0400

    Update infectbin.c

--- infectbin.c
+++ infectbin.c
@@ -1,9 +1,6 @@
 /*
 * 	infectbin.c  
 * 
-* 	Joao Guilherme Victorino aka pl4nkt0n
-* 	jgvictorino1 [at] gmail
-* 
 * 	This program is free software: you can redistribute it and/or modify
 * 	it under the terms of the GNU General Public License as published by
 * 	the Free Software Foundation, either version 3 of the License, or
